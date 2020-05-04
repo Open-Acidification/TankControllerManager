4b64d0106a76285f16eb035e381ee0ad8a79a4b7
Cameron Bierwagen <cqbierwagen@gmail.com>
2020-05-04 10:48:06 -0700

    So fragile

--- oa_server/devices/views.py
+++ oa_server/devices/views.py
@@ -3,8 +3,8 @@ import csv
 import platform
 import subprocess
 import re
-import requests
 from datetime import datetime
+import requests
 from django.shortcuts import render
 from django.http import HttpResponse, JsonResponse
 from django.views.decorators.csrf import csrf_exempt
