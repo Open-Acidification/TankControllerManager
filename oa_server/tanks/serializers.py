43407bd12b716f11941a43933a77e74fd0c2c38e
Cameron Bierwagen <cqbierwagen@gmail.com>
2020-05-21 14:19:46 -0700

    Lint

--- oa_server/tanks/serializers.py
+++ oa_server/tanks/serializers.py
@@ -1,5 +1,8 @@
 from rest_framework import serializers
 
+# Pylint wants create() and update() to be overriden,
+# but we don't need them here, so ignore the error.
+#pylint: disable=abstract-method
 class TankHistorySerializer(serializers.Serializer):
     time = serializers.DateTimeField(format='%Y/%m/%d %H:%M:%S')
     mac = serializers.CharField(max_length=17)
--- oa_server/tanks/views.py
+++ oa_server/tanks/views.py
@@ -1,10 +1,10 @@
 from django.shortcuts import render
 from django.views.decorators.http import require_http_methods
 from django.http import HttpResponse, JsonResponse
+from django.db import connection
 from tanks.serializers import TankHistorySerializer
 from devices.models import Device, Datum
 from devices.views import get_constraints, query_data, create_csv
-from django.db import connection
 
 @require_http_methods(["GET"])
 def get_tank_data(request, tankid):
