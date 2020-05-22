7a1d690cf948e224f29b8957ff491199c5e98461
Cameron Bierwagen <cqbierwagen@gmail.com>
2020-05-22 08:46:08 -0700

    Fix error in DevicesConfig.ready()

--- oa_server/devices/apps.py
+++ oa_server/devices/apps.py
@@ -5,12 +5,15 @@ class DevicesConfig(AppConfig):
     name = 'devices'
 
     def ready(self):
-        # The Django docs recommend against interacting with the database here,
-        # but this is the only way to ensure that all locks are released.
-
         # Pylint warns against this, but this is necessary as it
         # prevents an AppRegistryNotReady error.
         #pylint: disable=import-outside-toplevel
         from .models import Device
-        for device in Device.objects.all():
-            device.unlock()
+
+        try:
+            # The Django docs recommend against interacting with the database here,
+            # but this is the only way to ensure that all locks are released.
+            for device in Device.objects.all():
+                device.unlock()
+        except (psycopg2.OperationalError, django.db.utils.OperationalError):
+            pass
