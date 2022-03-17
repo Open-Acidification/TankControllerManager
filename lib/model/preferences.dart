23009a038424381509f9505dabf97e9ec9028ebe
Kaelan Willauer <49886320+KaelanWillauer@users.noreply.github.com>
2022-03-16 20:47:53 -0700

    Renamed tankList

--- lib/model/preferences.dart
+++ lib/model/preferences.dart
@@ -5,17 +5,17 @@ import 'dart:convert';
 import 'package:tank_manager/model/app_data.dart';
 import 'package:tank_manager/model/tank.dart';
 
-saveObj1(tanksList) async {
+saveObj1(tankList) async {
   SharedPreferences prefs = await SharedPreferences.getInstance();
-  prefs.setString('obj1', jsonEncode(tanksList));
+  prefs.setString('obj1', jsonEncode(tankList));
 }
 
 getObj1(BuildContext context) async {
-  if (Provider.of<AppData>(context, listen: false).tanksList.isNotEmpty) return;
+  if (Provider.of<AppData>(context, listen: false).tankList.isNotEmpty) return;
   SharedPreferences prefs = await SharedPreferences.getInstance();
   if (prefs.containsKey('obj1')) {
     String obj1 = prefs.getString('obj1')!;
-    Provider.of<AppData>(context, listen: false).tanksList =
+    Provider.of<AppData>(context, listen: false).tankList =
         List<Tank>.from(jsonDecode(obj1).map((obj1) => Tank.fromJson(obj1)));
   }
 }
