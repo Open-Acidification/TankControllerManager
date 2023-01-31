b6f90ac6e8707e30f12dbc63a0d95e316b69ae2c
Evan Taylor <suocesky@gmail.com>
2023-01-31 01:20:36 -0800

    Test writing in progress

--- test/app_data_test.dart
+++ test/app_data_test.dart
@@ -9,35 +9,30 @@ void main() async {
   TestWidgetsFlutterBinding.ensureInitialized();
   SharedPreferences.setMockInitialValues({});
   var appData = AppData.instance;
+
+  tearDown(() {
+    // Reset so that we no longer have a tankList
+    appData.tankList.clear();
+  });
+
   test('App current index', () {
     expect(appData.currentIndex, 0);
     appData.currentIndex = 3;
     expect(appData.currentIndex, 3);
   });
 
-  test('App delete tank list', () {
+  test('App add and set current tank list', () {
     expect(appData.tankList, []);
     appData.addTank(Tank('Tank', '192.168.0.1'));
     expect(appData.tankList[0], Tank('Tank', '192.168.0.1'));
-    appData.removeTank(Tank('Tank', '192.168.0.1'));
-    expect(appData.tankList, []);
-  });
-
-  test('App tank list', () {
-    expect(appData.tankList, []);
-    appData.addTank(Tank('Tank', '192.168.0.1'));
-    expect(appData.tankList[0], Tank('Tank', '192.168.0.1'));
-  });
-
-  test('App Tank current', () {
     appData.currentTank = Tank('Tank', '192.168.0.1');
     expect(appData.currentTank, Tank('Tank', '192.168.0.1'));
   });
 
   test('App write tank list', () async {
+    expect(appData.tankList, []);
     List<Tank> tankList = [Tank('Tank', '192.168.0.1')];
     appData.writeTankList(tankList);
-
     SharedPreferences prefs = await SharedPreferences.getInstance();
     expect(prefs.getString('obj1'), '[{"name":"Tank","ip":"192.168.0.1"}]');
   });
@@ -49,5 +44,12 @@ void main() async {
 
     await appData.readTankList();
     expect(appData.tankList, [Tank('Tank', '192.168.0.2')]);
+  });
+
+  test('App delete tank list', () {
+    appData.addTank(Tank('Tank', '192.168.0.1'));
+    expect(appData.tankList[0], Tank('Tank', '192.168.0.1'));
+    appData.removeTank(Tank('Tank', '192.168.0.1'));
+    expect(appData.tankList, []);
   });
 }
